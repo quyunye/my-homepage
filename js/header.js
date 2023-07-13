4d391a06ff5c42cc75a3acba6307578a766a9689
Ethan Qu <i@quyunye.com>
2023-07-12 21:54:17 -0700

    接入shi.la

--- js/header.js
+++ js/header.js
@@ -2,8 +2,6 @@ document.write('<!-----------  公共js和css 开始----------->\n');
 document.write(
     '<link rel="stylesheet" href="css/mouse.css">\n' +
     '<link rel="stylesheet" href="css/iconfont.css" type="text/css">\n' +
-    '<link rel="apple-touch-icon" sizes="76x76" href="images/logo.png">\n' +
-    '<link rel="shortcut icon" href="images/logo.png" type="image/x-png">\n' +
     '<link rel="stylesheet" href="css/page.css">\n');
 document.write('<!-----------  公共js和css 结束----------->\n');
 
